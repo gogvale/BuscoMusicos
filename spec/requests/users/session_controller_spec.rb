f4d9572b5889e8d50352dcfdf14a76f2e1c9556c
Gabriel Oliveira <gogvale2011@gmail.com>
2022-10-17 10:38:04 -0500

    Add session tests to a single file

--- spec/requests/users/session_controller_spec.rb
+++ spec/requests/users/session_controller_spec.rb
@@ -2,7 +2,9 @@
 
 require 'rails_helper'
 
-RSpec.describe Users::RegistrationController, type: :request do
+RSpec.describe 'SessionControllers', type: :request do
+  let(:password) { Faker::Internet.password }
+  let(:user) { create(:musician, password:) }
   describe 'Sign Up' do
     let(:new_password) { Faker::Internet.password }
     describe 'for musicians' do
@@ -183,6 +185,76 @@ RSpec.describe Users::RegistrationController, type: :request do
           end
         end
       end
+    end
+  end
+  describe 'Sign In' do
+    describe 'with correct information' do
+      let(:params) do
+        {
+          email: user.email,
+          password:
+        }
+      end
+      before do
+        post '/users/sign_in', params:
+      end
+      it 'returns http success' do
+        expect(response).to(have_http_status(:success))
+      end
+      it 'returns token' do
+        expect(response.headers.dig('Access-Token')).not_to(be_blank)
+        expect(response.headers.dig('Expire-At')).not_to(be_blank)
+        expect(response.headers.dig('Refresh-Token')).not_to(be_blank)
+      end
+    end
+    describe 'with incorrect information' do
+      let(:params) do
+        {
+          email: user.email,
+          password:
+        }
+      end
+      context 'missing password' do
+        before do
+          params[:password] = nil
+          post '/users/sign_up', params:
+        end
+        it 'returns http unprocessable_entity' do
+          expect(response).to(have_http_status(:unprocessable_entity))
+        end
+      end
+      context 'incorrect password' do
+        before do
+          params[:password] = Faker::Internet.password
+          post '/users/sign_up', params:
+        end
+        it 'returns http unprocessable_entity' do
+          expect(response).to(have_http_status(:unprocessable_entity))
+        end
+      end
+      context 'non-existent email' do
+        before do
+          params[:email] = Faker::Internet.email
+          post '/users/sign_up', params:
+        end
+        it 'returns http unprocessable_entity' do
+          expect(response).to(have_http_status(:unprocessable_entity))
+        end
+      end
+    end
+  end
+  describe 'Refresh Token' do
+    let(:headers) { Support::User.new(expired: true).headers }
+    before do
+      post '/users/tokens', headers:
+    end
+    it 'returns http success' do
+      expect(response).to(have_http_status(:success))
+    end
+    it 'refreshes token' do
+      expect(response.headers.dig('Access-Token')).not_to(be_blank)
+      expect(response.headers.dig('Expire-At')).not_to(be_blank)
+      expect(response.headers.dig('Refresh-Token')).not_to(be_blank)
     end
   end
 end
